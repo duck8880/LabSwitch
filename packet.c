37c477ac0f2710fc399d10d926886402b1170fe5
Jianqiu Cao <jianqiu_cao@163.com>
2018-03-27 22:14:07 -1000

    new file:   .gitignore 	new file:   README 	new file:   circles.config 	new file:   h4s1.config 	new file:   host.c 	new file:   host.h 	new file:   main.c 	new file:   main.h 	new file:   makefile 	new file:   man.c 	new file:   man.h 	new file:   net.c 	new file:   net.h 	new file:   p2p.config 	new file:   packet.c 	new file:   packet.h 	new file:   switch.c 	new file:   switch.h

---
+++ packet.c
@@ -0,0 +1,99 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+
+#include "main.h"
+#include "packet.h"
+#include "net.h"
+#include "host.h"
+
+
+void packet_send(struct net_port *port, struct packet *p)
+{
+char msg[PAYLOAD_MAX+4];
+int i;
+
+if (port->type == PIPE) {
+	msg[0] = (char) p->src; 
+	msg[1] = (char) p->dst;
+	msg[2] = (char) p->type;
+	msg[3] = (char) p->length;
+	for (i=0; i<p->length; i++) {
+		msg[i+4] = p->payload[i];
+	}
+	write(port->pipe_send_fd, msg, p->length+4);
+//printf("PACKET SEND, src=%d dst=%d p-src=%d p-dst=%d\n", 
+//		(int) msg[0], 
+//		(int) msg[1], 
+//		(int) p->src, 
+//		(int) p->dst);
+}
+
+return;
+}
+
+int packet_recv(struct net_port *port, struct packet *p)
+{
+char msg[PAYLOAD_MAX+4];
+int n;
+int i;
+	
+if (port->type == PIPE) {
+	n = read(port->pipe_recv_fd, msg, PAYLOAD_MAX+4);
+	if (n>0) {
+		p->src = (char) msg[0];
+		p->dst = (char) msg[1];
+		p->type = (char) msg[2];
+		p->length = (int) msg[3];
+		for (i=0; i<p->length; i++) {
+			p->payload[i] = msg[i+4];
+		}
+
+// printf("PACKET RECV, src=%d dst=%d p-src=%d p-dst=%d\n", 
+//		(int) msg[0], 
+//		(int) msg[1], 
+//		(int) p->src, 
+//		(int) p->dst);
+	}
+}
+
+return(n);
+}
+
+
+int send_tree_packet(int node_port_num, int node_id, char node_type, struct net_port **node_port, 
+		int localRootID, int localRootDist, int localParent) {
+	int k;
+	struct payload_tree_packet *payload_tree;
+	struct packet *new_packet;
+
+	for (k = 0; k < node_port_num; k++) { /* Scan all ports */
+		// Create the outgoing tree packet
+		new_packet = (struct packet *) malloc(sizeof(struct packet));
+		new_packet->src = (char) node_id;
+		new_packet->dst = 0;
+		new_packet->type = PKT_TREE_PACKET;
+		new_packet->length = sizeof(struct payload_tree_packet);
+	
+		// Create the payload of the tree packet
+		payload_tree = (struct payload_tree_packet *)new_packet->payload;
+		payload_tree->rootID = (char)localRootID;
+		payload_tree->rootDist = localRootDist;
+		payload_tree->senderType = node_type;
+		if (k == localParent) {
+			payload_tree->senderChild = 'Y';
+		} else {
+			payload_tree->senderChild = 'N';
+		}
+	
+		// Send the packet
+		packet_send(node_port[k], new_packet);
+		free(new_packet);
+	}
+}
+
+
+
---
+++ switch.c
@@ -0,0 +1,264 @@
+ /*
+  * switch.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "main.h"
+#include "net.h"
+#include "man.h"
+#include "switch.h"
+#include "packet.h"
+
+#define MAX_MSG_LENGTH 100
+#define PKT_PAYLOAD_MAX 100
+#define TENMILLISEC 10000   /* 10 millisecond sleep */
+#define FORWARDING_TABLE_SIZE 100
+#define TREE_PKT_INTVL	100000	// The interval between sending two tree packets
+
+// Look up the destination in the forwading table and update the table
+// Return the number of out port
+int look_up_update_ftable(struct forwarding_table_entry forwarding_table[], 
+	struct packet *in_packet, int in_port) {
+
+	int k = 0;
+	int out_port = -1;
+	int recorded = 0;
+
+	// Go through the forwarding table for finding the port 
+	// corresponding to the destination node.
+	while (forwarding_table[k].valid == 1 && k < FORWARDING_TABLE_SIZE) {
+		if (in_packet->dst == forwarding_table[k].dest) {
+			// Out port number matches a forwarding table entry
+			// Choose this port for forwarding the packet
+			out_port = forwarding_table[k].port;
+		}
+		if (in_packet->src == forwarding_table[k].dest) {
+			// If the source node has been recorded in the table,
+			// set the flag.
+			recorded = 1;
+		}
+		k++;
+	}
+
+	if (recorded == 0 && k < FORWARDING_TABLE_SIZE) {	
+		// The source node hasn't been recorded
+		forwarding_table[k].valid = 1;
+		forwarding_table[k].dest = in_packet->src;
+		forwarding_table[k].port = in_port;
+		printf("Swtich debug: recorded node %d at port %d in the forwarding table\n", 
+			in_packet->src, in_port);
+	}
+
+	// Print out the forwarding table for debugging
+	/*
+	k = 0;
+	printf("Swtich debug: forwarding table:\n");
+	printf("Valid\tDest\tPort\t\n");
+	while (forwarding_table[k].valid == 1 && k < FORWARDING_TABLE_SIZE) {
+		printf("%d\t%d\t%d\t\n", forwarding_table[k].valid, 
+			forwarding_table[k].dest, forwarding_table[k].port);
+		k++;
+	}
+	*/
+	return out_port;
+	
+}	
+
+// Update localRootID, localRootDist, and localParent
+int update_tree_info(struct packet *in_packet, int k, int *localRootID, 
+		int *localRootDist, int *localParent) {
+	struct payload_tree_packet *payload_tree;
+	
+	payload_tree = (struct payload_tree_packet *)in_packet->payload;
+	
+	if (payload_tree->senderType == 'S') {
+		if (payload_tree->rootID < *localRootID) { // Found a better root
+			*localRootID = payload_tree->rootID; // Node becomes the child of the neighbor at port k
+			*localParent = k;
+			*localRootDist = payload_tree->rootDist + 1; // New distance = neighbor distance + one hop to neighbor
+		} else if (payload_tree->rootID == *localRootID) { // The root is the same
+			if (*localRootDist > payload_tree->rootDist + 1) { // Found a better path to the root
+				*localParent = k;
+				*localRootDist = payload_tree->rootDist + 1; // New distance = neighbor dist + one hop to neighbor
+			}
+		}
+	}
+
+	return 1;
+}
+
+// Update status of local port k, whether it’s the tree or not.
+int update_port_tree(struct packet *in_packet, int *localPortTree, int k, int localParent) {
+	struct payload_tree_packet *payload_tree;
+
+	payload_tree = (struct payload_tree_packet *)in_packet->payload;
+	
+	if (payload_tree->senderType == 'H') { // Port is attached to a host, so it’s part of the tree
+		localPortTree[k] = 1;
+	} else if (payload_tree->senderType == 'S') { // Port is attached to a switch
+		if (localParent == k) { // Port is attached to the parent, so it’s part of the tree
+			localPortTree[k] = 1;
+		} else if (payload_tree->senderChild == 'Y') { // Port is attached to a child, so it’s part of the tree
+			localPortTree[k] = 1;
+		} else {
+			localPortTree[k] = 0;
+		}
+	} else {
+		localPortTree[k] = 0;
+	}
+
+	return 1;
+}
+
+
+
+/*
+ *  Main 
+ */
+
+void switch_main(int switch_id)
+{
+
+/* State */
+
+struct net_port *node_port_list;
+struct net_port **node_port;  // Array of pointers to node ports
+int node_port_num;            // Number of node ports
+
+int i, k, n;
+
+struct packet *in_packet; /* Incoming packet */
+
+struct net_port *p;
+
+struct forwarding_table_entry forwarding_table[FORWARDING_TABLE_SIZE];
+int out_port;
+
+// For spanning tree
+int localRootID = switch_id;
+int localRootDist = 0;
+int localParent = -1;	// The port number of the parent
+int *localPortTree;
+int treePacketCnt = 0;
+
+
+/*
+ * Initialize pipes 
+ */
+
+/*
+ * Create an array node_port[ ] to store the network link ports
+ * at the host.  The number of ports is node_port_num
+ */
+node_port_list = net_get_port_list(switch_id);
+
+/*  Count the number of network link ports */
+node_port_num = 0;
+for (p=node_port_list; p!=NULL; p=p->next) {
+	node_port_num++;
+}
+	/* Create memory space for the array */
+node_port = (struct net_port **) 
+	malloc(node_port_num*sizeof(struct net_port *));
+
+	/* Load ports into the array */
+p = node_port_list;
+for (k = 0; k < node_port_num; k++) {
+	node_port[k] = p;
+	p = p->next;
+}	
+
+// Initialize the localPortTree array, none of the
+// ports is a part of the tree
+localPortTree = malloc(node_port_num * sizeof(int));
+for (k = 0; k < node_port_num; k++) {
+	localPortTree[k] = 0;
+}
+
+// Initialize the forwarding table
+for (k = 0; k < FORWARDING_TABLE_SIZE; k++) {
+	forwarding_table[k].valid = 0;
+	forwarding_table[k].dest = 0;
+	forwarding_table[k].port = 0;
+}
+
+while(1) {
+	
+	/*
+	 * Get packets from incoming links and translate to jobs
+  	 * Put jobs in job queue
+ 	 */
+
+	for (k = 0; k < node_port_num; k++) { /* Scan all ports */
+
+		in_packet = (struct packet *) malloc(sizeof(struct packet));
+		n = packet_recv(node_port[k], in_packet);
+
+		if (n > 0){
+			// Look up the destination in the forwading table,
+			// and update the forwarding table
+			out_port = look_up_update_ftable(forwarding_table, in_packet, k);
+
+			if (in_packet->type == PKT_TREE_PACKET) {
+				// Update localRootID, localRootDist, and localParent
+				update_tree_info(in_packet, k, &localRootID, &localRootDist, &localParent);
+				
+				// Update status of local port k, whether it’s the tree or not.
+				update_port_tree(in_packet, localPortTree, k, localParent);
+				
+			} else {
+				if (in_packet->dst != switch_id) {	// The destination is another node.
+					// Forward the packet
+					if (out_port != -1) {
+						// Node isn't found in the forwarding table 
+						// Send packet to the destination port
+						packet_send(node_port[out_port], in_packet);
+						printf("Swtich %d debug: packet is sent to node %d from port %d\n", 
+							switch_id, in_packet->dst, out_port);
+					} else {
+						// Broadcast packet
+						for (i = 0; i < node_port_num; i++) {
+							if (i != k && localPortTree[i] == 1) {	
+								// Send to ports only in the tree
+								// Don't send to the incoming port
+								packet_send(node_port[i], in_packet);
+								printf("Swtich %d debug: packet going to node %d is boardcasted from port %d\n", 
+									switch_id, in_packet->dst, i);
+							}
+						}
+						
+					}
+				}
+			}
+
+			free(in_packet);
+		}
+		else {
+			free(in_packet);
+		}
+	}
+
+	// Send the tree packet
+	if (++treePacketCnt == TREE_PKT_INTVL / TENMILLISEC) {
+		send_tree_packet(node_port_num, switch_id, 'S', node_port, 
+			localRootID, localRootDist, localParent);
+		treePacketCnt = 0;
+	}
+
+	/* The host goes to sleep for 10 ms */
+	usleep(TENMILLISEC);
+
+} /* End of while loop */
+
+}
+
+
+
+
